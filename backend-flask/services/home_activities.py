dbd6337157b80d46172e0a6438d0b18bc3279bb7
Mohamed <Mohamed.dine95@gmail.com>
2023-03-18 18:10:31 +0000

    editing out code

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta, timezone
+
 from opentelemetry import trace
 
 tracer = trace.get_tracer("home.activities")
@@ -10,7 +10,7 @@ class HomeActivities:
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
 
-      
+
       results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
