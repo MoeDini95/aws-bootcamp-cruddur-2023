1c5ef1abeb53af98ea2fa6ef98982efc7f04ee77
Mohamed <Mohamed.dine95@gmail.com>
2023-04-21 23:06:23 +0000

    seed data edits

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' , 'test@email' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' , 'test@email' ,'MOCK');
+  ('Andrew Brown', 'andrewbrown' , 'MOCK'),
+  ('Andrew Bayko', 'bayko' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
